5385385655c843fe96bd4b0466d65e308c4c279c
whoana <whoana@gmail.com>
2021-03-02 13:04:04 +0900

    commit segue project

---
+++ TrafficSegues/TrafficSegues/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by whoana on 2021/03/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue){
+        
+    }
+}
+
